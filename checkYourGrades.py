8f3d8f1fc3f969db1741a45d61479621dd14d954
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-07-18 16:28:36 -0400

    returns the name of the course to its object.

--- checkYourGrades.py
+++ checkYourGrades.py
@@ -1,6 +1,7 @@
 def setCourseName():
     print("Set the name of the course,")
     courseName = input(">> ")
+    return courseName
 
 ## So things like homework, classwork, exams, attendance, etc...
 def setCourseItems():
