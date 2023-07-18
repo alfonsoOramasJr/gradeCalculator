cc3dd63148766c2aff7aef76940ce592f091c2b6
alfonso oramas <100045145+workordeath@users.noreply.github.com>
2023-07-18 16:27:02 -0400

    first commit

---
+++ checkYourGrades.py
@@ -0,0 +1,16 @@
+def setCourseName():
+    print("Set the name of the course,")
+    courseName = input(">> ")
+
+## So things like homework, classwork, exams, attendance, etc...
+def setCourseItems():
+    pass
+
+def addCourseItem(courseName):
+    pass
+
+def removeCourseItem(courseName):
+    pass
+
+def findCourseItem(courseName):
+    pass
